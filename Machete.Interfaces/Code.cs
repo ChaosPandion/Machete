d8a898d461e6542bae0d5eadd7f71e2121794c97
ChaosPandion <ChaosPandion+GitHub@gmail.com>
2010-12-17 10:53:56 -0500

    Partial commit of changes to the compiler.

---
+++ Machete.Interfaces/Code.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Machete.Interfaces
+{
+    public delegate IDynamic Code(IEnvironment environment, IArgs args);
+}
