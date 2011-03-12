872a4530bbcd79b11077a809ca21f5a14a16bc09
ChaosPandion <ChaosPandion@gmail.com>
2011-03-12 01:11:54 -0500

    Added more Lexical Convention tests.

---
+++ Machete.Tests/Lexical Conventions/Comments.cs
@@ -0,0 +1,24 @@
+﻿using Machete.Core;
+using Xunit;
+
+namespace Machete.Tests.Lexical_Conventions
+{
+    public sealed class Comments : TestBase
+    {
+        [Fact(DisplayName = "07.04 Comments: SingleLineComment")]
+        public void Test0704A()
+        {
+            var r = Engine.ExecuteScriptToDynamic("//A single line comment\n2");
+            Assert.IsAssignableFrom<INumber>(r);
+            Assert.Equal(2.0, ((INumber)r).BaseValue);
+        }
+
+        [Fact(DisplayName = "07.04 Comments: MultiLineComment")]
+        public void Test0704B()
+        {
+            var r = Engine.ExecuteScriptToDynamic("/*A multi \n line comment*/\n2");
+            Assert.IsAssignableFrom<INumber>(r);
+            Assert.Equal(2.0, ((INumber)r).BaseValue);
+        }
+    }
+}
---
+++ Machete.Tests/Lexical Conventions/LineTerminators.cs
@@ -0,0 +1,40 @@
+﻿using Machete.Core;
+using Xunit;
+
+namespace Machete.Tests.Lexical_Conventions
+{
+    public sealed class LineTerminators : TestBase
+    {
+        [Fact(DisplayName = "07.03 Line Terminators: LF")]
+        public void Test0703A()
+        {
+            var r = Engine.ExecuteScriptToDynamic("\u000A2");
+            Assert.IsAssignableFrom<INumber>(r);
+            Assert.Equal(2.0, ((INumber)r).BaseValue);
+        }
+
+        [Fact(DisplayName = "07.03 Line Terminators: CR")]
+        public void Test0703B()
+        {
+            var r = Engine.ExecuteScriptToDynamic("\u000D2");
+            Assert.IsAssignableFrom<INumber>(r);
+            Assert.Equal(2.0, ((INumber)r).BaseValue);
+        }
+
+        [Fact(DisplayName = "07.03 Line Terminators: LS")]
+        public void Test0703C()
+        {
+            var r = Engine.ExecuteScriptToDynamic("\u20282");
+            Assert.IsAssignableFrom<INumber>(r);
+            Assert.Equal(2.0, ((INumber)r).BaseValue);
+        }
+
+        [Fact(DisplayName = "07.03 Line Terminators: PS")]
+        public void Test0703D()
+        {
+            var r = Engine.ExecuteScriptToDynamic("\u20292");
+            Assert.IsAssignableFrom<INumber>(r);
+            Assert.Equal(2.0, ((INumber)r).BaseValue);
+        }
+    }
+}
